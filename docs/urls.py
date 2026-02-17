e5789fce8be3b4625f63cabe11cbcc7ee97364d9
SorokineDmytro <dmytro.sorokine@gmail.com>
2026-02-17 16:23:27 +0100

    feat: static css file added && templates created extending base.html

--- docs/urls.py
+++ docs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import space_list_view, space_details_view, page_details_view
 
+app_name = "docs"
+
 urlpatterns = [
-    path("", space_list_view),
-    path("<slug:space_slug>", space_details_view),
-    path("<slug:space_slug>/<slug:page_slug>", page_details_view),
+    path("", space_list_view, name="space_list"),
+    path("<slug:space_slug>", space_details_view, name="space_details"),
+    path("<slug:space_slug>/<slug:page_slug>", page_details_view, name="page_details"),
 ]
